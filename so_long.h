a4872cb0481857e63d85c55c9493bc9f05f8df3c
adamoski13 <adamjurczyk92@gmail.com>
2025-07-01 19:19:11 +0200

    Update so_long.h

--- so_long.h
+++ so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ajurczyk <ajurczyk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adamjurczyk <adamjurczyk@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 10:46:51 by ajurczyk          #+#    #+#             */
-/*   Updated: 2025/07/01 15:14:44 by ajurczyk         ###   ########.fr       */
+/*   Updated: 2025/07/01 19:14:57 by adamjurczyk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@
 # define KEY_ESC 65307
 # define WIN_CLOSE 17
 
-typedef struct s_data
+typedef struct s_list
 {
 	void	*win;
 	void	*mlx;
@@ -50,53 +50,54 @@ typedef struct s_data
 	int		m_width;
 	int		m_height;
 	int		fd;
-	int		p_x;
-	int		p_y;
-	int		e_x;
-	int		e_y;
+	int		player_x;
+	int		player_y;
+	int		exit_x;
+	int		exit_y;
 	int		scr_x;
 	int		scr_y;
-}				t_data;
+}				t_list;
 
 //errors
-void	ft_free_exit(char *msg, t_data *game, int code);
-void	ft_free(t_data *game);
+void	ft_free_exit(char *msg, t_list *game);
+void	ft_free_mem(t_list *game);
 
-//utils
-void	set_exit(t_data *game);
-void	set_player_pos(t_data *game);
-void	check_members(t_data *game);
-void	init_map(t_data *game);
-void	find_height(t_data *game);
+//set_utils
+void	set_player_and_exit(t_list *game);
+void	init_map(t_list *game);
+void	find_height(t_list *game);
+
+//checks
+void	check_file(t_data *game);
+void	check_map(t_data *game);
+void	check_structure(t_list *game);
+void	check_path(t_data *game, int y, int x);
+void	validate_path(t_data *game);
 
 //libft_utils
 size_t	get_digits(int n);
-void	ft_putstr(char *str);
+void	ft_putstr_fd(char *str);
 char	*ft_strchr(char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(char *s);
+//libft_utils_2
 size_t	ft_strlen(char *s);
 int		ft_strcmp(char *s1, char *s2);
 char	*ft_itoa(int n);
 char	*ft_substr(char *s, unsigned int start, size_t len);
-void	*ft_calloc(size_t num, size_t n);
-char	*ft_strtrim(char *src, char *set);
 
 //game_utils
-void	start_data(t_data *game);
-void	add_images(t_data *game);
-void	print_bg(t_data *game);
-void	print_nonmovings(t_data *game);
-void	print_player(t_data *game);
+void	start_game(t_list *game);
+void	print_background(t_list *game);
+void	print_others(t_list *game);
 
 //move_utils
-int		close_window_hook(t_data *game);
-int		key_hook(int key, t_data *game);
-void	ft_winloseprint(int is_win);
-void	refresh_frame(t_data *game);
-void	move_u_d(t_data *game, int val);
-void	move_l_r(t_data *game, int val);
-void	endgame(t_data *game, int is_win);
+int		close_window_hook(t_list *game);
+int		key_hook(int key, t_list *game);
+void	refresh_frame(t_list *game);
+void	move_u_d(t_list *game, int val);
+void	move_l_r(t_list *game, int val);
+void	endgame(t_list *game, int is_win);
 
 //other
 char	*get_next_line(int fd);
